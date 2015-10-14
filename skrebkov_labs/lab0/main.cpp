347eec3ab716f586b5a16c36edefb59f742c9af2
ArtemSkrebkov <skrebkov.art@gmail.com>
2015-10-14 22:59:42 +0300

    Added lab0

---
+++ skrebkov_labs/lab0/main.cpp
@@ -0,0 +1,86 @@
+#include <opencv2\core.hpp>
+#include <opencv2\imgproc.hpp>
+#include <opencv2\highgui.hpp>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+const string helper = "lab0 --image <filename>  --mode <mode>,\
+					  where <filename> image filename,\
+					  <mode> processing mode (may be 'blur', 'houghLines', 'findContours')";
+
+bool processCommandLine(int argc, char *argv[], string &filename, string &mode);
+
+int main(int argc, char *argv[]) {
+	string filename, mode;
+
+	if (!processCommandLine(argc, argv, filename, mode)) {
+		cout << helper << endl;	
+		return 1;
+	}
+
+	Mat image = imread(filename, CV_LOAD_IMAGE_COLOR);
+
+	Mat result;
+	if (mode.compare("blur") == 0) {
+		blur(image, result, Size(5, 5));
+	} else if (mode.compare("houghLines") == 0) {
+		cvtColor(image, image, CV_BGR2GRAY);
+
+		vector<Vec2f> lines;
+		HoughLines(image, lines, 1, CV_PI/180, 100, 0, 0 );
+
+		result = Mat::zeros(image.size(), CV_8UC3);
+		for(size_t i = 0; i < lines.size(); i++)
+		{
+			float rho = lines[i][0], theta = lines[i][1];
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a*rho, y0 = b*rho;
+			pt1.x = cvRound(x0 + 1000*(-b));
+			pt1.y = cvRound(y0 + 1000*(a));
+			pt2.x = cvRound(x0 - 1000*(-b));
+			pt2.y = cvRound(y0 - 1000*(a));
+			line(result, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+		}
+	} else if (mode.compare("findContours") == 0) {
+		cvtColor(image, image, CV_BGR2GRAY);
+
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		findContours(image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+		result = Mat::zeros( image.size(), CV_8UC3 );
+		for(size_t i = 0; i < contours.size(); i++ )
+		{
+			drawContours(result, contours, i, Scalar(0, 0, 255), 2, 8, hierarchy, 0, Point() );
+		}
+	} else {
+		cout << "Wrong mode." << endl;
+	}
+
+	if (!result.empty()) {
+		imshow("Result", result);
+	}
+
+	waitKey(0);
+
+	return 0;
+}
+
+bool processCommandLine(int argc, char *argv[], string &filename, string &mode) {
+	for (int i = 1; i < argc; i++)
+	{
+		string str = argv[i];
+		if (str.compare("--image") == 0) {
+			filename = argv[i + 1];
+		} else if (str.compare("--mode") == 0)
+		{
+			mode = argv[i + 1];
+		}
+	}
+
+	return true;
+}
